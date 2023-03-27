6a2e5bda60a4e7833caa4a8d3c522927f978d583
Mark Jensen <maje@trifork.com>
2023-03-27 10:37:58 +0200

    Update dto.rs

--- src/dto.rs
+++ src/dto.rs
@@ -3,6 +3,22 @@ use uuid::Uuid;
 use chrono::NaiveDateTime;
 
 #[derive(Serialize, Deserialize)]
+struct Payload {
+    html: String,
+    text: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Channel {
+    id: Uuid,
+    name: String,
+    icon: String,
+    description: String,
+    visibiliy: bool,
+    size: i32,
+}
+
+#[derive(Serialize, Deserialize)]
 struct Message {
     id: Uuid,
     timestamp: NaiveDateTime,
@@ -11,7 +27,19 @@ struct Message {
 }
 
 #[derive(Serialize, Deserialize)]
-struct Payload {
-    html: String,
-    text: String,
+struct User {
+    id: Uuid,
+    name: String,
+    status: String,
+    from_system: Uuid,
+    avatar: String,
+}
+
+
+#[derive(Serialize, Deserialize)]
+struct System {
+    id: Uuid,
+    address: String,
+    last_sync: NaiveDateTime,
+    status: String,
 }
