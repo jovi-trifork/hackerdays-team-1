f5a7191f7d209254b4be8273ed2d11efaa786684
Joakim Vinter-Olesen <jovi@trifork.com>
2023-03-27 14:00:41 +0200

    merge

--- src/model.rs
+++ src/model.rs
@@ -5,7 +5,6 @@ use std::{
     sync::{Arc, RwLock},
 };
 
-<<<<<<< HEAD
 type Uuid = String;
 type ChannelId = String;
 type UserId = String;
@@ -14,28 +13,16 @@ type Channels = Arc<RwLock<HashMap<ChannelId, Channel>>>;
 type Users = Arc<RwLock<HashMap<UserId, User>>>;
 type Systems = Arc<RwLock<HashMap<String, Vec<System>>>>;
 type ChannelUsers = Arc<RwLock<HashMap<ChannelId, Vec<UserId>>>>;
-=======
-type ChannelMessages = Arc<RwLock<HashMap<String, Vec<Message>>>>;
 type UserChannels = Arc<RwLock<HashMap<String, Vec<Channel>>>>;
-type Channels = Arc<RwLock<HashMap<String, Channel>>>;
-type Users = Arc<RwLock<HashMap<String, User>>>;
-type ChannelUsers = Arc<RwLock<HashMap<String, Vec<String>>>>;
-type Systems = Arc<RwLock<HashMap<String, Vec<System>>>>;
->>>>>>> 465373efddd940d2fab84034ace25158743c4382
 
 #[derive(Clone, Default)]
 pub struct AppState {
     pub messages: ChannelMessages,
     pub channels: Channels,
     pub users: Users,
-<<<<<<< HEAD
-    pub systems: Systems,
-    pub channel_users: ChannelUsers,
-=======
     pub user_channels: UserChannels,
     pub channel_users: ChannelUsers,
     pub systems: Systems,
->>>>>>> 465373efddd940d2fab84034ace25158743c4382
 }
 
 #[derive(Serialize, Deserialize, Clone)]
